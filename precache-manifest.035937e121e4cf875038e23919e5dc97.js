1a835ab3c9db6c2403223bf00410cd2573cd65e3
Klajdi Avdiaj <klajdi.avdiaj@smart-tech.al>
2019-05-24 13:51:51 +0200

    Updates

---
+++ precache-manifest.035937e121e4cf875038e23919e5dc97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e716301b3a02b8b719b447ba99de923",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "18eef9b9960011e936d8",
+    "url": "/todo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d999b26ffca7391d7912",
+    "url": "/todo/static/css/main.391a951e.chunk.css"
+  },
+  {
+    "revision": "18eef9b9960011e936d8",
+    "url": "/todo/static/js/2.fc3dc2b5.chunk.js"
+  },
+  {
+    "revision": "d999b26ffca7391d7912",
+    "url": "/todo/static/js/main.181a4604.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
